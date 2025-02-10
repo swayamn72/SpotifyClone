16ac65f7fce10906803166095f6cddeb272ded60
Swayam Nakte <swayam.nakte@somaiya.edu>
2025-02-10 08:17:10 +0530

    7th commit

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 let currentSong = new Audio();
 
+function secondsToMinutesSeconds(seconds){
+    if(isNaN(seconds) || seconds<0){
+        return "Invalid Input";
+    }
+
+    const minutes = Math.floor(seconds/60)
+    const remainingSeconds = Math.floor(seconds%60)
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
+
 async function getSongs() {
     
     let a = await fetch("http://127.0.0.1:5500/songs")
@@ -32,6 +46,7 @@ const playMusic = (track)=>{
     // let audio = new Audio("/songs/" + track + ".mp3")
     currentSong.src = "/songs/" + track + ".mp3"
     currentSong.play()
+    play.src = "pause.svg"
 }
 
 async function main() {
@@ -69,6 +84,24 @@ async function main() {
                 playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim())
             })
             
+        })
+
+        play.addEventListener("click", ()=>{
+            if(currentSong.paused){
+                currentSong.play();
+                play.src = "pause.svg"
+            }
+            else{
+                currentSong.pause()
+                play.src = "playPlaybar.svg"
+            }
+        })
+
+        currentSong.addEventListener("timeupdate", ()=>{
+            console.log(currentSong.currentTime, currentSong.duration)
+            document.querySelector(".songTime").innerHTML = 
+            `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`;
+            document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration)*100 + "%"
         })
 }
 
