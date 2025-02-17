f9fdf7ddf2681ab0f0407eda8bd816f7eedc35c9
Swayam Nakte <swayam.nakte@somaiya.edu>
2025-02-17 17:26:10 +0530

    8th commit

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let currentSong = new Audio();
+let gaana;
 
 function secondsToMinutesSeconds(seconds){
     if(isNaN(seconds) || seconds<0){
-        return "Invalid Input";
+        return "00:00";
     }
 
     const minutes = Math.floor(seconds/60)
@@ -44,6 +45,7 @@ async function getSongs() {
 
 const playMusic = (track)=>{
     // let audio = new Audio("/songs/" + track + ".mp3")
+    
     currentSong.src = "/songs/" + track + ".mp3"
     currentSong.play()
     play.src = "pause.svg"
@@ -53,7 +55,7 @@ async function main() {
 
     
     
-    let gaana = await getSongs();
+    gaana = await getSongs();
     console.log(gaana); 
 
     
@@ -98,10 +100,43 @@ async function main() {
         })
 
         currentSong.addEventListener("timeupdate", ()=>{
-            console.log(currentSong.currentTime, currentSong.duration)
             document.querySelector(".songTime").innerHTML = 
             `${secondsToMinutesSeconds(currentSong.currentTime)} / ${secondsToMinutesSeconds(currentSong.duration)}`;
             document.querySelector(".circle").style.left = (currentSong.currentTime / currentSong.duration)*100 + "%"
+        })
+
+        document.querySelector(".seekBar1").addEventListener("click", e=>{
+            let percent = (e.offsetX/e.target.getBoundingClientRect().width) * 100;
+            document.querySelector(".circle").style.left = percent + "%";
+            currentSong.currentTime = (currentSong.duration) * percent / 100;
+        })
+
+        document.querySelector(".hamburger").addEventListener("click", ()=>{
+            document.querySelector(".left").style.left = "0"
+        })
+
+        document.querySelector(".close").addEventListener("click", ()=>{
+            document.querySelector(".left").style.left = "-120%"
+        })
+
+        // previous.addEventListener()
+        previous.addEventListener("click", ()=>{
+            let index = gaana.indexOf(currentSong.src);
+            console.log(index)
+            if(index-1>=0){
+                playMusic(gaana[index-1].substr(28).replaceAll("%20"," ").replaceAll(".mp3", ""))
+            }
+        })
+        next.addEventListener("click", ()=>{
+            let index = gaana.indexOf(currentSong.src);
+            console.log(index)
+            if(index+1<gaana.length){
+                playMusic(gaana[index+1].substr(28).replaceAll("%20"," ").replaceAll(".mp3", ""))
+            }
+        })
+
+        document.querySelector(".range").getElementsByTagName("input")[0].addEventListener("change", (e)=>{
+            currentSong.volume = parseInt(e.target.value)/100;
         })
 }
 
